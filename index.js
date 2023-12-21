455a60aff3b89a8b5e70664c55190d2cf4b267b5
Andy Phua <andyphuawc@gmail.com>
2023-12-21 10:51:26 +0800

    Add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -10,6 +11,7 @@ morgan.token('body', req => {
 
 const requestLogger = morgan(':method :url :status :res[content-length] - :response-time ms :body')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(requestLogger)
